2dfe0c029e439b35251d6899eba41225fc25f6f3
eerotuo <45943412+eerotuo@users.noreply.github.com>
2019-09-19 17:38:19 +0300

    VK2
    2.1

--- src/index.js
+++ src/index.js
@@ -1,72 +1,20 @@
 import "./styles.css";
+import { type } from "os";
 var kierros = 0;
+var width = 0;
+var VarTimeout;
+var time = (document.getElementById("board").innerHTML = `
+<h1>Tic Tac Toe</h1>
 
-document.getElementById("board").innerHTML = `
-<h1>Hello Vanilla!</h1>
-
-<style>
-
-table#t01 {
-  //width: 100%;
-  border: 1px solid black;
-  background-color: #f1f1c1;
-  border-collapse: collapse;
-  
-}
-
-tr, th, td{
-  border: 1px solid black;
-  width: 100px;
-  height: 100px;
-  text-align: center;
-}
-</style>
-
-<table id="t01">
-  <tr>
-    <td></td>
-    <td></td> 
-    <td></td>
-    <td></td>
-    <td></td>
-  </tr>
-  <tr>
-    <td></td>
-    <td></td> 
-    <td></td>
-    <td></td>
-    <td></td>
-  </tr>
-  <tr>
-    <td></td>
-    <td></td> 
-    <td></td>
-    <td></td>
-    <td></td>
-  </tr>
-  <tr>
-    <td></td>
-    <td></td> 
-    <td></td>
-    <td></td>
-    <td></td>
-  </tr>
-  <tr>
-    <td></td>
-    <td></td> 
-    <td></td>
-    <td></td>
-    <td></td>
-  </tr>
-</table>
-
-`;
+`);
 
 var table = document.getElementById("t01");
 if (table != null) {
   for (var i = 0; i < table.rows.length; i++) {
     for (var j = 0; j < table.rows[i].cells.length; j++)
       table.rows[i].cells[j].onclick = function() {
+        resetTimeOut();
+        VarTimeout = setTimeout(timeOutAlert, 10000);
         tableText(this);
         checkWinnerHorisontal(table);
         checkWinnerVertical(table);
@@ -76,11 +24,27 @@ if (table != null) {
 }
 
 function tableText(tableCell) {
+  var vaaka = tableCell.parentElement.rowIndex;
+  var pysty = tableCell.cellIndex;
+  timerii();
+  timeri2();
+  timer++;
+
+  console.log(vaaka + "testi" + pysty);
+
   if (kierros % 2 === 0) {
     tableCell.innerHTML = "X";
+    tableCell.style.backgroundColor = "rgb(124, 252, 0)";
+    tableCell.style.fontSize = "30px";
+    tableCell.style.fontFamily = "Helvetica";
+
     kierros++;
   } else {
     tableCell.innerHTML = "O";
+    tableCell.style.backgroundColor = "rgb(250, 128, 114)";
+    tableCell.style.fontSize = "30px";
+    tableCell.style.fontFamily = "Helvetica";
+
     kierros++;
   }
   //alert(tableCell.innerHTML);
@@ -152,7 +116,7 @@ function checkWinnerDiagonal(table) {
     }
 
     if (table.rows[i].cells[i].innerHTML === "O") {
-      lkmx++;
+      lkmo++;
       if (lkmx === 5) {
         alert("Player 2 won!");
         break;
@@ -173,11 +137,75 @@ function checkWinnerDiagonal(table) {
     }
 
     if (table.rows[i].cells[4 - i].innerHTML === "O") {
-      lkmx++;
+      lkmo++;
       if (lkmx === 5) {
         alert("Player 2 won!");
         break;
       }
     }
   }
+}
+
+function timeri2() {
+  if (timer == 0) {
+    var myVar = setInterval(move, 10);
+  }
+}
+
+function timeOutAlert() {
+  alert("Timeout");
+  width = 0;
+  kierros++;
+}
+
+function resetTimeOut() {
+  clearTimeout(VarTimeout);
+  width = 0;
+}
+
+function resetBar() {
+  var elem = document.getElementById("myBar");
+  elem.style.width = width + "%";
+}
+
+function move() {
+  var elem = document.getElementById("myBar");
+  width++;
+
+  var widthpros = (width / 1000) * 100;
+  elem.style.width = widthpros + "%";
+  //elem.innerHTML = width * 1 + "/10";
+}
+
+var aloitus = "";
+var min = 0;
+var sec = 0;
+var hour = 0;
+var timer = 0;
+
+function timerii() {
+  if (timer == 0) {
+    var myVar = setInterval(myTimer, 1000);
+  }
+}
+
+function myTimer() {
+  if (aloitus === "") {
+    aloitus = new Date();
+    console.log(aloitus);
+  }
+
+  if (sec < 59) {
+    sec++;
+  } else if (sec === 59) {
+    sec = 0;
+    min++;
+  } else if (min === 60) {
+    sec = 0;
+    min = 0;
+    hour++;
+  }
+
+  document.getElementById("demo").innerHTML =
+    hour + "h " + min + "min " + sec + "sec";
 }
